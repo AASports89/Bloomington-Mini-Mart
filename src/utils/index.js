fbcbdd7513f7bbe5dabbf3035680a421ffb18cbf
AASports89 <aavakyan16@students.kgi.edu>
2026-01-04 04:14:37 -0800

    updated 01.04.26

--- src/utils/index.js
+++
@@ -1,9 +0,0 @@
-const User = require('./User');
-const express = require('express');
-const router = express.Router();
-
-router.get('/', function(_req, res) {
-    res.send("Welcome to Costco Roscoe's Liquor & Gas™")
-})
-
-module.exports = { User, router };
