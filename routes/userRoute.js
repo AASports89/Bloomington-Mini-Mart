b021a0ab912dddf5bc5cbf031999eeb44da983ec
AASports89 <aavakyan16@students.kgi.edu>
2026-01-06 20:32:46 -0800

    update 01.06.25

--- routes/userRoute.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { userAuth } from "../src/middleware/userAuth.js";
-import { getUserData } from "../src/controller/userController.js";
-
-const userRouter = express.Router();
-// Endpoint for get users data
-userRouter.get('/data', userAuth, getUserData);
-
-export default userRouter;
